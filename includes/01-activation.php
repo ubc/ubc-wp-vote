62e6dc80a023ba5261d32311e50df0651e07cf74
Kelvin <kelvin@longevitygraphics.com>
2020-04-08 12:24:15 -0700

    Update plugin version number on options table.

--- includes/01-activation.php
+++ includes/01-activation.php
@@ -12,4 +12,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die;
 }
 
+// Update plugin version number on options table.
+update_option( 'ubc_wp_vote_db_id', UBC_WP_VOTE_VERSION );
+
+// Create required database tables if not already exist.
 \UBC\CTLT\WPVote\WP_Vote_DB::create_tables();
