a0ada96aceedf0d915f43fd09c32a0db36feca29
Kelvin <kelvin@longevitygraphics.com>
2020-04-08 09:15:43 -0700

    Initial plugin setup

---
+++ includes/01-deactivation.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * This file will run during plugin deactivation.
+ *
+ * @package ubc_wp_vote
+ */
+
+namespace UBC\CTLT\WPVote\Deactivation;
+
+// If this file is called directly, abort.
+if ( ! defined( 'ABSPATH' ) ) {
+	die;
+}
