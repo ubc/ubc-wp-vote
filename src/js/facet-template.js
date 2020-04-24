f9ab5daac6c9b5ae302b5e4fc5c18eb0896652cf
Kelvin <kelvin@longevitygraphics.com>
2020-04-24 11:15:23 -0700

    remove facet filter title, add it inside Gutenberg

--- src/js/facet-template.js
+++ src/js/facet-template.js
@@ -1,19 +1,3 @@
-// Scripts from facetwp to add facet labels above filters https://facetwp.com/add-labels-above-each-facet/.
-(function( $ ) {
-	$(document).on('facetwp-loaded', function() {
-		$('.facetwp-facet').each(function() {
-			var $facet = $(this);
-			var facet_name = $facet.attr('data-name');
-			var facet_label = FWP.settings.labels[facet_name];
-
-			if ($facet.closest('.facet-wrap').length < 1 && $facet.closest('.facetwp-flyout').length < 1) {
-				$facet.wrap('<div class="facet-wrap"></div>');
-				$facet.before('<span class="facet-label">' + facet_label + '</span>');
-			}
-		});
-	});
-})(jQuery);
-
 // update and trigger rating number range field when dropdown changes and update posts rendered.
 (function() {
     document.addEventListener('change', function ( event ) {
